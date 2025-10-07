4d41089fdaeab474385b80d7181e074ddb09f21a
Santiago Pampillon <120607419+QuaaDev@users.noreply.github.com>
2025-10-07 03:19:38 -0300

    feedback de seleccion de unidad

--- Script/administrador_mundo.gd
+++ Script/administrador_mundo.gd
@@ -15,16 +15,23 @@ func _input(event):
 			if event.button_index == MOUSE_BUTTON_LEFT: #Si se apreta el mouse izq
 				if event.pressed:
 					if mouse_sobre_unidad != null and unidad_a_mover == null:
+						#Se selecciona la unidad a interactuar
 						unidad_a_mover = mouse_sobre_unidad
+						unidad_a_mover.siendo_movido()
 						print("Almaceno unidad")
 					elif mouse_sobre_unidad != unidad_a_mover and unidad_a_mover != null:
+						#Se selecciona la casilla a moverse
 						print("muevo unidad")
+						unidad_a_mover.ya_no_me_mueven()
 						mover_unidad(unidad_a_mover)
 						unidad_a_mover = null
 					elif mouse_sobre_unidad != null and mouse_sobre_unidad == unidad_a_mover:
+						#Se cancela el movimiento
 						print("Cancelo movimiento")
+						unidad_a_mover.ya_no_me_mueven()
 						unidad_a_mover = null
 					else:
+						#Error
 						print("Click Izquierdo condicion no reconocida, valores: ")
 func mover_unidad(unidad : Node2D):
 	var coordenadas_mouse = tile_map.coordenada_global_del_mouse_a_tilemap()
--- Script/unidad.gd
+++ Script/unidad.gd
@@ -1,12 +1,25 @@
 extends Node2D
 @onready var nodo_mundo = self.get_parent()
 @onready var tile_map: Node2D = $"../TileMap"
+
+@export var equipo : int = 0
+
 func _ready() -> void:
+	self.add_to_group(str(equipo))
+	print(self.get_groups())
 	var coordenada_local_tilemap = tile_map.local_to_map(position)
 	var coordenada_global = tile_map.map_to_local(coordenada_local_tilemap)
 	self.position = coordenada_global #Centra a la unidad en la celda
 	#Terminar esto luego, el codigo no funciona prq el nodo "tilemap" es un node2d, no un nodo XD
 	pass
+
+
+
+func siendo_movido() -> void:
+	self.modulate = Color(18.892, 0.0, 0.0, 0.448)
+	
+func ya_no_me_mueven() -> void:
+	self.modulate = Color(1.0, 1.0, 1.0, 1.0)
 
 
 func _on_area_2d_mouse_entered() -> void:
