ee7ea3f5773a2a5b7bf33dc3729f811d9b6c0a04
Santiago Pampillon <120607419+QuaaDev@users.noreply.github.com>
2025-10-13 18:22:38 -0300

    traduccion al ingles

--- Script/globales/algoritmo_dijkstra.gd
+++ Script/globales/algoritmo_dijkstra.gd
@@ -2,7 +2,7 @@ extends Node
 var tile_map : Node2D
 var tile_map_hud : TileMapLayer
 var tile_map_base : TileMapLayer
-var movimientos_disponibles : Dictionary #Almacena el resultado del algoritmo Dijkstra para su posterior uso
+var movimientos_disponibles : Dictionary #Stores the result of the Dijkstra algorithm for later use
 
 func dibujando_tile_map(ubicaciones : Dictionary) -> void:
 	for i in ubicaciones:
@@ -11,37 +11,37 @@ func dibujando_tile_map(ubicaciones : Dictionary) -> void:
 func dibujando_tile_individual(ubicacion : Vector2) -> void:
 	tile_map_hud.set_cell(ubicacion,3,Vector2(0,0),0)
 	
-func obtener_coste_movimiento_tile(coordenadas : Vector2) -> int: #Obtiene el coste de movimiento de cada tile
+func obtener_coste_movimiento_tile(coordenadas : Vector2) -> int: #Gets the movement cost of each tile
 	var data = tile_map_base.get_cell_tile_data(coordenadas)
 	if data:
 		return data.get_custom_data("resistencia_movimiento")
 	else:
-		print("Error data no encontrada obtener_coste_movimiento_tile")
+		print("Error data not found  obtener_coste_movimiento_tile")
 		return 1
 
 func moviendo_unidad(unidad : Node2D) -> void:
-	limpiar_movimientos() #Limpia la anterior lista de movimientos
+	limpiar_movimientos() #Clear the previous move list
 	var start = unidad.coordenada_local_tilemap
 	var cantidad_de_movimiento_maximo = unidad.puntos_movimiento
-	var frontier = [] #Almacena las fronteras que hay que explorar
-	frontier.append(start) #Donde empieza la ejecucion
-	var reached = {} #Almacena las casillas ya exploradas
-	reached[start] = 0#Almacena la cantidad de puntos de movimiento que consume
-	while frontier.size() > 0: #Mientras existan mas fronteras:
-		var current = frontier.pop_front() #Selecciona la primera frontera y la elimina del array
+	var frontier = [] #Store the frontiers to be explored
+	frontier.append(start) #Where the execution begins
+	var reached = {} #Stores already explored squares
+	reached[start] = 0##Stores the amount of movement points it consumes
+	while frontier.size() > 0: #As long as there are more borders:
+		var current = frontier.pop_front() #Selects the first border and removes it from the array
 		
-		var distancia_actual = reached[current]#Almacena la distancia que se recorrio desde start
+		var distancia_actual = reached[current]#Stores the distance traveled since start
 		
-		if distancia_actual >= cantidad_de_movimiento_maximo: #Si alcanza el limite de movimiento
-			continue#Omite esta ejecucion del while
+		if distancia_actual >= cantidad_de_movimiento_maximo: #If it reach the movement limit
+			continue#Skip this while execution
 		
-		for next in get_neighbors(current):#Obtiene todos los vecinos de la ubicacion actual
+		for next in get_neighbors(current):#Get all neighbors of the current location
 			var nuevo_costo = distancia_actual + obtener_coste_movimiento_tile(next)
-			if not reached.has(next) or nuevo_costo < reached[next]:#Si el nodo ya fue explorado, lo omite
+			if not reached.has(next) or nuevo_costo < reached[next]:#If the node has already been explored OR the current movement value is less than the one already stored:
 				reached[next] = nuevo_costo
-				frontier.append(next)#Agrega la ubicacion como nueva frontera, para que luego se expanda en base a este
+				frontier.append(next)#Add the location as a new border, so that it will then expand based on this
 	dibujando_tile_map(reached)
-	movimientos_disponibles = reached.duplicate() #Almacena los movimientos disponibles
+	movimientos_disponibles = reached.duplicate() #Stores available moves
 		
 func get_neighbors(origen : Vector2) -> Array: #Devuelve la lista de vecinos de X tile hex
 	#Modelo odd-q
