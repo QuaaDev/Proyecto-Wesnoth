dacf918eceae7887074553cccef7dfd1dae57b1b
Santiago Pampillon <120607419+QuaaDev@users.noreply.github.com>
2025-11-07 02:19:21 -0300

    Script que maneja el tamaño del VBox

---
+++ Script/HUD/v_box_prueba.gd
@@ -0,0 +1,17 @@
+extends VBoxContainer
+
+@onready var nombre: Label = $Nombre
+@onready var identificador: Label = $Identificador
+
+var word = "Pepe pica palos pero pepe no sabe de palos asique pepe se queda sin palo" #Texto que se aplicara al label
+func _ready() -> void:
+	var text_to_print = "" #Variable temporal que almacena el texto del label
+	var x = 1#Contador de ejecuciones
+	var quantity_of_letters = 22#Cada cuantas letras se agrega salto de linea
+	for i in range(1,ceil(float(word.length())/quantity_of_letters)+1):
+		#Cada 22 caracteres, hace un bucle (empezando siempre por 1)
+		text_to_print = text_to_print + word.substr((x-1)*quantity_of_letters,quantity_of_letters) + "\n"
+		#Substrae X cantidad de letras apartir del caracter numero (ejecuciones-1)* cada cuantas letras hace salto de linea
+		x += 1
+	text_to_print = text_to_print.rstrip("\n") #Elimina el salto de linea sobrante
+	identificador.text = text_to_print#Remplaza el texto del label
