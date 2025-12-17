7c8dc8f91bcb26f3f8ada3c769d4d337201447b0
Santiago Pampillon <120607419+QuaaDev@users.noreply.github.com>
2025-12-17 16:35:50 -0300

    recurso defensa agregado

---
+++ Script/Unidad/recursos defensa/estadisticas_defensa.gd
@@ -0,0 +1,4 @@
+extends Node
+class_name estadisticas_defensa
+
+@export var informacion_defensa : EstadisticaDefensaRes
---
+++ Script/Unidad/recursos defensa/estadisticas_defensa_res.gd
@@ -0,0 +1,6 @@
+extends Resource
+class_name EstadisticaDefensaRes
+
+@export var punzante : int #0
+@export var cortante : int#1
+@export var contundente: int#2
