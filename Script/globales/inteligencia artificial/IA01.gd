14560d1bbd3c81007248228140dd0bd2ff6c9b41
Santiago Pampillon <120607419+QuaaDev@users.noreply.github.com>
2026-02-10 18:26:52 -0300

    Update IA01.gd

--- Script/globales/inteligencia artificial/IA01.gd
+++ Script/globales/inteligencia artificial/IA01.gd
@@ -1,22 +1,26 @@
 extends Node
 signal todas_las_unidades_procesadas #Cuando se agota la lista de unidades a aplicarle IA, activa esta señal
 var nodo_mundo
-#Obtener las unidades a las que hay que aplicarle la IA
-func cargar_unidades(equipo:int) -> Array:#Devuelve las unidades dentro del grupo especificado
-	return get_tree().get_nodes_in_group(str(equipo))
-	
+var ubicaciones_ocupadas_enemigos
+
 func ejecutar_ia(equipo : int):
 	var unidades = cargar_unidades(equipo) #Almacena las unidades a aplicarle IA
 	obtener_datos(unidades)
 
+#Obtener las unidades a las que hay que aplicarle la IA
+func cargar_unidades(equipo:int) -> Array:#Devuelve las unidades dentro del grupo especificado
+	return get_tree().get_nodes_in_group(str(equipo))
+	
 func obtener_datos(unidades : Array):
 	#print(unidad.name)
 	#AlgoritmoDijkstra.moviendo_unidad(unidad, nodo_mundo.ubicaciones_ocupadas,false) 
 	#Carga los movimientos legales en el algoritmo dirksdad, se accede con AlgoritmoDijkstra.movimientos_disponibles
-	var ubicaciones_ocupadas = nodo_mundo.ubicaciones_ocupadas.duplicate()
+	ubicaciones_ocupadas_enemigos = nodo_mundo.ubicaciones_ocupadas.duplicate()
+	#Almacena las ubicaciones enemigas 
 	for i in unidades:
-		if i.coordenada_local_tilemap in ubicaciones_ocupadas:
-			pass
+		if i.coordenada_local_tilemap in ubicaciones_ocupadas_enemigos:
+		#Quita de la lista a las unidades aliadas para facilitar el encontrar enemigos
+			ubicaciones_ocupadas_enemigos.erase(i.coordenada_local_tilemap)
 		print(i.name)
 		print(i.coordenada_local_tilemap)
 func obtener_nodo_mundo (nodo : Node):
