92e6c9b79be126f26f2b04f90d2e3e58298fc272
Santiago Pampillon <120607419+QuaaDev@users.noreply.github.com>
2026-02-16 18:30:44 -0300

    loop de la IA actualizado.
    Ahora en vez de procesar todas las unidades a la vez, se procesa una por una en orden.

--- Script/globales/inteligencia artificial/IA01.gd
+++ Script/globales/inteligencia artificial/IA01.gd
@@ -6,18 +6,22 @@ var movimientos_disponibles_incluyendo_ocupados
 var unidades_almacenadas
 func ejecutar_ia(equipo : int):
 	unidades_almacenadas = cargar_unidades(equipo) #Almacena las unidades a aplicarle IA
-	obtener_datos(unidades_almacenadas)
-	ejecutar_ataque(unidades_almacenadas)
+	ubicaciones_ocupadas_enemigos = nodo_mundo.ubicaciones_ocupadas.duplicate() #Actualiza la informacion
+	descartar_unidades_aliadas(unidades_almacenadas)
+	for i in unidades_almacenadas:
+		print("-------------------")
+		print(i.name)
+		obtener_datos(i)
+		ejecutar_ataque(i)
+		print("-------------------")
 
 #Obtener las unidades a las que hay que aplicarle la IA
 func cargar_unidades(equipo:int) -> Array:#Devuelve las unidades dentro del grupo especificado
 	return get_tree().get_nodes_in_group(str(equipo))
 	
-func obtener_datos(unidades : Array):
-	ubicaciones_ocupadas_enemigos = nodo_mundo.ubicaciones_ocupadas.duplicate()
+func obtener_datos(unidad : unidad_base):
 	#Almacena las ubicaciones enemigas 
-	descartar_unidades_aliadas(unidades)
-	obtener_posibles_objetivos(unidades)
+	obtener_posibles_objetivos(unidad)
 #---------------Recursos Obtener Datos---------------
 func descartar_unidades_aliadas(unidades : Array):
 	#Quita de la lista a las unidades aliadas para facilitar el encontrar enemigos
@@ -25,35 +29,31 @@ func descartar_unidades_aliadas(unidades : Array):
 		if i.coordenada_local_tilemap in ubicaciones_ocupadas_enemigos:
 			ubicaciones_ocupadas_enemigos.erase(i.coordenada_local_tilemap)
 			
-func obtener_posibles_objetivos(unidades : Array):
-	for i in unidades:#explora todas las unidades
-		print("Ubicaciones Ocupadas Por Enemigos ", i.name)
-		AlgoritmoDijkstra.moviendo_unidad(i, nodo_mundo.ubicaciones_ocupadas,false)#Carga la lista de movimientos para esta unidad
-		movimientos_disponibles_incluyendo_ocupados = AlgoritmoDijkstra.movimientos_disponibles_incluyendo_ocupados.duplicate()
-		#Duplica la lista de movimientos disponibles blabla
-		if movimientos_disponibles_incluyendo_ocupados.is_empty():
-			#Si la lista esta vacia, se ahorra el procesar las cosas
-			print("No se detecto ubicaciones de enemigos")
-		else:
-			for x in movimientos_disponibles_incluyendo_ocupados: #Por cada posible ataque
-				if x in ubicaciones_ocupadas_enemigos:#Verifica que no sean aliados prq obviamente no atacas aliados
-					print("La unidad ",x," esta a rango de ataque")
-					i.objetivos_a_atacar[x] = movimientos_disponibles_incluyendo_ocupados[x] #Guarda la informacion en la unidad
-		#print(movimientos_disponibles_incluyendo_ocupados)
-		print("objetivos a atacar",i.objetivos_a_atacar)
-		print(i.coordenada_local_tilemap)
-		print("-------------------------")
+func obtener_posibles_objetivos(unidad : unidad_base):
+	print("Ubicaciones Ocupadas Por Enemigos ")
+	AlgoritmoDijkstra.moviendo_unidad(unidad, nodo_mundo.ubicaciones_ocupadas,false)#Carga la lista de movimientos para esta unidad
+	movimientos_disponibles_incluyendo_ocupados = AlgoritmoDijkstra.movimientos_disponibles_incluyendo_ocupados.duplicate()
+	#Duplica la lista de movimientos disponibles blabla
+	if movimientos_disponibles_incluyendo_ocupados.is_empty():
+		#Si la lista esta vacia, se ahorra el procesar las cosas
+		print("No se detecto ubicaciones de enemigos")
+	else:
+		for x in movimientos_disponibles_incluyendo_ocupados: #Por cada posible ataque
+			if x in ubicaciones_ocupadas_enemigos:#Verifica que no sean aliados prq obviamente no atacas aliados
+				print("La unidad ",x," esta a rango de ataque")
+				unidad.objetivos_a_atacar[x] = movimientos_disponibles_incluyendo_ocupados[x] #Guarda la informacion en la unidad
+	#print(movimientos_disponibles_incluyendo_ocupados)
+	print("objetivos a atacar",unidad.objetivos_a_atacar)
+	print(unidad.coordenada_local_tilemap)
 
-func ejecutar_ataque(unidades:Array):
-	for i in unidades:
-		if !i.objetivos_a_atacar.is_empty():
-			analizar_ataque(i)
-			#realizar_movimiento_adyacente(i)
-		else:
-			print("No hay posibles ataques :c")
+func ejecutar_ataque(unidad: unidad_base):
+	if !unidad.objetivos_a_atacar.is_empty():
+		analizar_ataque(unidad)
+		realizar_movimiento_adyacente(unidad)
+	else:
+		print("No hay posibles ataques :c")
 
 func analizar_ataque(unidad : unidad_base):
-	print(unidad.name)
 	var clave_random = unidad.objetivos_a_atacar.keys().pick_random()
 	var objetivo : Dictionary
 	objetivo[clave_random] = unidad.objetivos_a_atacar[clave_random]
