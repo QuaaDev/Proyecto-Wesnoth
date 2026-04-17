8bd01583cf98545b654b0f09cc7193c089b64c28
Santiago Pampillon <120607419+QuaaDev@users.noreply.github.com>
2026-04-17 13:08:50 -0300

    configurando los flip
    se a configurado el vflip, hflip y diagonal flip. Tambien se sumo un asset que demuestra el resultado visual

--- Escenas/tilemap/probando_autotile/prueba_rotacion_tile.gd
+++ Escenas/tilemap/probando_autotile/prueba_rotacion_tile.gd
@@ -2,7 +2,7 @@ extends TileMapLayer
 @onready var button: Button = $Button
 var coordenadas = Vector2i(0, 0)
 var source_id = 2
-var atlas_coordenadas = Vector2i(0,0)
+var atlas_coordenadas = Vector2i(1,0)
 var alternative_id = 0
 var contador = -1
 enum TileTransform {
@@ -15,29 +15,28 @@ enum FlipEnum{
 	fliph = TileSetAtlasSource.TRANSFORM_FLIP_H,
 	flipv = TileSetAtlasSource.TRANSFORM_FLIP_V,
 	flipv_and_h = TileSetAtlasSource.TRANSFORM_FLIP_V | TileSetAtlasSource.TRANSFORM_FLIP_H,
-	fliph_and_v = TileSetAtlasSource.TRANSFORM_FLIP_H | TileSetAtlasSource.TRANSFORM_FLIP_V
 }
 func rotar():
 	contador += 1
 	if contador == 0:
-		print("fliph")
+		print("horizontal flip")
 		set_cell(coordenadas, source_id, atlas_coordenadas, alternative_id | 0) #Devuelve la orientacion original
 		set_cell(coordenadas, source_id, atlas_coordenadas, alternative_id | FlipEnum.fliph)#aplica fliph
+		#0,0,1,1,1,0 -> 1,1,0,0,0,1 horizontal flip 
 	elif contador == 1:
-		print("flipv")
+		print("vertical flip")
 		set_cell(coordenadas, source_id, atlas_coordenadas, alternative_id | 0)#Devuelve la orientacion original
 		set_cell(coordenadas, source_id, atlas_coordenadas, alternative_id | FlipEnum.flipv)#aplica flipv
+		#,1,1,1,0,0,0 -> 0,0,0,1,1,1 vertical flip
 	elif contador == 2:
-		print("flipv and fliph")
+		print("diagonal flip")
 		set_cell(coordenadas, source_id, atlas_coordenadas, alternative_id | 0)#Devuelve la orientacion original
 		set_cell(coordenadas, source_id, atlas_coordenadas, alternative_id | FlipEnum.flipv_and_h)#aplica flipv
-		contador = -1
+		#0,1,1,1,0,0 -> 1,0,0,0,1,1 diagonal flip
 	elif contador == 3:
-		print("fliph and flipv")
-		set_cell(coordenadas, source_id, atlas_coordenadas, alternative_id | 0)#Devuelve la orientacion original
-		set_cell(coordenadas, source_id, atlas_coordenadas, alternative_id | FlipEnum.fliph_and_v)#aplica flipv
+		print("Original position")
+		set_cell(coordenadas, source_id, atlas_coordenadas, alternative_id | 0)
 		contador = -1
-	
 #https://docs.godotengine.org/en/stable/classes/class_tilesetatlassource.html#constants
 #Las rotaciones de 90 y 270 no me sirven de nada prq rompe la forma del hexagono, tienen que ser siempre de 0 o 180
 #Puedo usar fliph y flipv de forma individual para cubrir 3 casos con un solo sprite! Hora de hacer todo de cero el template...
