adee32ddc3c1abda2864b7a8657999d3404818bd
Santiago Pampillon <120607419+QuaaDev@users.noreply.github.com>
2026-04-18 16:37:23 -0300

    cargando los layers con el recurso

--- Escenas/tilemap/probando_autotile/prueba_rotacion_tile.gd
+++ Escenas/tilemap/probando_autotile/prueba_rotacion_tile.gd
@@ -45,8 +45,13 @@ func aplicar_terreno():
 			var custom_data = get_cell_tile_data(coordenada)
 			var tipo_terreno_id = custom_data.get_custom_data("tipo_terreno_id")#Almacena el id del terreno
 			var contador_posicion_bit := 0#Almacena cual bit del terreno es el que se esta analizando
-			for i in range(0,7):#Va del 0 al 6
-				print(i)
+			var vecinos = get_neighbors(coordenada)#Almacena los vecinos en formato 0,1,2,3,4,5 del template
+			for i in vecinos:#Va del 0 al 5
+				if !(get_cell_source_id(i) == -1):
+					#Si el tile vecino es invalido, lo saltea 
+					print("Vecino sin tile definido, aplicando continue")
+					continue
+				
 			#set_cell(coordenada, source_id, atlascoordenada, alternative_id | FlipEnum.fliph)#aplica fliph
 			print("vecinos de :",coordenada," son: ")
 			print("Mi Modelo: ", get_neighbors(coordenada))
