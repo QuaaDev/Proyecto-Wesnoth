63dde0e36f5d4288826bea356f1b8a86ba62e6ea
Santiago Pampillon <120607419+QuaaDev@users.noreply.github.com>
2026-04-20 16:10:26 -0300

    Update prueba_rotacion_tile.gd

--- Escenas/tilemap/probando_autotile/prueba_rotacion_tile.gd
+++ Escenas/tilemap/probando_autotile/prueba_rotacion_tile.gd
@@ -10,18 +10,6 @@ extends TileMapLayer
 @onready var Layer5: TileMapLayer = $TerrainCarpet/Layer5
 #--------------------------------------
 @export var limite_del_mapa : Vector2i #Define los limites del mapa para limitar los algoritmos
-var coordenadas = Vector2i(0, 0)
-#var source_id = 5
-#var atlas_coordenadas = [Vector2i(0,0),Vector2i(1,0),Vector2i(2,0),Vector2i(3,0),Vector2i(4,0),Vector2i(5,0),
-#Vector2i(6,0),Vector2i(7,0),Vector2i(0,1),Vector2i(1,1),Vector2i(2,1),Vector2i(3,1),Vector2i(4,1),Vector2i(5,1)]
-#var alternative_id = 0
-var contador = -1
-enum TileTransform {
-	ROTATE_0 = 0,
-	ROTATE_90 = TileSetAtlasSource.TRANSFORM_TRANSPOSE | TileSetAtlasSource.TRANSFORM_FLIP_H,
-	ROTATE_180 = TileSetAtlasSource.TRANSFORM_FLIP_H | TileSetAtlasSource.TRANSFORM_FLIP_V,
-	ROTATE_270 = TileSetAtlasSource.TRANSFORM_TRANSPOSE | TileSetAtlasSource.TRANSFORM_FLIP_V,
-}
 enum FlipEnum{
 	fliph = TileSetAtlasSource.TRANSFORM_FLIP_H,
 	flipv = TileSetAtlasSource.TRANSFORM_FLIP_V,
@@ -29,21 +17,24 @@ enum FlipEnum{
 }
 func rotar():
 	aplicar_terreno()
-	#prueba_rotaciones()
-
+func _ready() -> void:
+	print(FlipEnum.fliph)
+	print(FlipEnum.flipv)
+	print(FlipEnum.flipv_and_h)
 func aplicar_terreno():
 	#Explora todas las coordenadas dentro del area del limite (incluyendo el limite)
 	#+1 para que incluya completamente el limite del mapa, si no queda 1 por debajo
 	for x in range(-1,limite_del_mapa.x+1):
 		for y in range(-1,limite_del_mapa.y+1):
-			var coordenada = Vector2i(x,y)
-			var source_id = get_cell_source_id(coordenada)
+			var coordenada = Vector2i(x,y)#Las coordenadas del hexagono actual
+			var source_id = get_cell_source_id(coordenada)#Obtiene su source id
 			if source_id == -1: #Si el tile actual es invalido, saltea su procesamiento
 				continue
 			#var atlas_coordenada = get_cell_atlas_coords(coordenada)
 			#var alternative_id = 0
-			var custom_data = get_cell_tile_data(coordenada)
-			var tipo_terreno_id = custom_data.get_custom_data("tipo_terreno_id")#Almacena el id del terreno
+			var tipo_terreno_id = get_cell_tile_data(coordenada).get_custom_data("tipo_terreno_id")#Almacena el id del terreno actual
+			#Los bit representan las fronteras que pueden tener los hexagonos -> (0,0,0,0,0,0)
+			#Un bit positivo es una frontera activa, la posicion de ese bit representa que parte del hexagono es
 			var contador_posicion_bit := 0#Almacena cual bit del terreno es el que se esta analizando
 			var vecinos = get_neighbors(coordenada)#Almacena los vecinos en formato 0,1,2,3,4,5 del template
 			#print("--------------------------")
@@ -54,7 +45,7 @@ func aplicar_terreno():
 				if get_cell_source_id(i) == -1:
 					#Si el tile vecino es invalido, lo saltea 
 					#print("Vecino sin tile definido, aplicando continue")
-					contador_posicion_bit += 1
+					contador_posicion_bit += 1#Avanza en uno la posicion del bit
 					continue
 				var vecino_tipo_terreno_id = get_cell_tile_data(i).get_custom_data("tipo_terreno_id")
 				if tipo_terreno_id != vecino_tipo_terreno_id:
@@ -86,43 +77,13 @@ func aplicar_terreno():
 							get(nombre_variable).set_cell(coordenada, 0, Vector2i(1,3), 0 | efecto_a_aplicar)
 						else:
 							get(nombre_variable).set_cell(coordenada, 0, Vector2i(2,3), 0 | efecto_a_aplicar)
-				contador_posicion_bit += 1
+				contador_posicion_bit += 1#Avanza en uno la posicion del bit
 				#print("mio ",tipo_terreno_id)
 				#print("vecino ",vecino_tipo_terreno_id)
 			#print("------------------------")
-			#set_cell(coordenada, source_id, atlascoordenada, alternative_id | FlipEnum.fliph)#aplica fliph
 			#print("vecinos de :", coordenada," son ",get_neighbors(coordenada))
 
-
-func prueba_rotaciones():
-	contador += 1
-	#if contador == 0:
-		#label.text = "horizontal flip"
-		#for i in atlas_coordenadas:
-			#set_cell(i, source_id, i, alternative_id | 0) #Devuelve la orientacion original
-			#set_cell(i, source_id, i, alternative_id | FlipEnum.fliph)#aplica fliph
-		##0,0,1,1,1,0 -> 1,1,0,0,0,1 horizontal flip 
-	#elif contador == 1:
-		#label.text = "vertical flip"
-		#for i in atlas_coordenadas:
-			#set_cell(i, source_id, i, alternative_id | 0)#Devuelve la orientacion original
-			#set_cell(i, source_id, i, alternative_id | FlipEnum.flipv)#aplica flipv
-			##,1,1,1,0,0,0 -> 0,0,0,1,1,1 vertical flip
-	#elif contador == 2:
-		#label.text = "diagonal flip"
-		#for i in atlas_coordenadas:
-			#set_cell(i, source_id, i, alternative_id | 0)#Devuelve la orientacion original
-			#set_cell(i, source_id, i, alternative_id | FlipEnum.flipv_and_h)#aplica flipv
-		##0,1,1,1,0,0 -> 1,0,0,0,1,1 diagonal flip
-	#elif contador == 3:
-		#label.text = "Original position"
-		#for i in atlas_coordenadas:
-			#set_cell(i, source_id, i, alternative_id | 0)
-		#contador = -1
-	
-#https://docs.godotengine.org/en/stable/classes/class_tilesetatlassource.html#constants
-#Las rotaciones de 90 y 270 no me sirven de nada prq rompe la forma del hexagono, tienen que ser siempre de 0 o 180
-#Puedo usar fliph y flipv de forma individual para cubrir 3 casos con un solo sprite! Hora de hacer todo de cero el template...
+#func aplicar_efecto(Layer : String) -> 
 func get_neighbors(origen : Vector2i) -> Array: #Devuelve la lista de vecinos de X tile hex
 	#Modelo odd-q
 	var vecinos = []
