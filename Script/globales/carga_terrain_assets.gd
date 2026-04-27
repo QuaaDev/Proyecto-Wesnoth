43bd4b59126498cadbe9fab40a36c427a1e88095
Santiago Pampillon <120607419+QuaaDev@users.noreply.github.com>
2026-04-27 18:02:16 -0300

    script global de terrenos

---
+++ Script/globales/carga_terrain_assets.gd
@@ -0,0 +1,13 @@
+extends Node
+class_name carga_terrain_assets
+
+const blanco : Dictionary = {
+	negro = "uid://d0a42yhqderap",#blanco-negro
+	rojo = "uid://m7wtfl36bpe5",#blanco-rojo
+	verde = "uid://dersk2j5ltlxq"#blanco-verde
+}
+const negro : Dictionary = {
+	blanco = "uid://dm3jpurp47x04", #negro-blanco
+	rojo = "uid://qxu7gbsjy6x0",#negro-rojo
+	verde = "uid://d4i7ljw0c5ovb"#negro-verde
+}
